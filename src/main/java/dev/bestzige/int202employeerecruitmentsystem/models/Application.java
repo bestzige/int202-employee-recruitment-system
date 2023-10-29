208b4e48a922afd10ccf13b7564996ffb6eaa7fd
BestZige <bestzige@hotmail.com>
2023-10-29 17:30:57 +0700

    Hello World

--- src/main/java/dev/bestzige/int202employeerecruitmentsystem/models/Application.java
+++ src/main/java/dev/bestzige/int202employeerecruitmentsystem/models/Application.java
@@ -1,2 +1,23 @@
-package dev.bestzige.int202employeerecruitmentsystem.models;public class Application {
+package dev.bestzige.int202employeerecruitmentsystem.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Application {
+    private static int idCounter = 0;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private JobPosition jobPosition;
+
+    public Application(String firstName, String lastName, String email, JobPosition jobPosition) {
+        this.id = idCounter++;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.jobPosition = jobPosition;
+    }
 }
